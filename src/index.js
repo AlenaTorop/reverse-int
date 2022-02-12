ffe8b788411634656c157cbcc679dce6e8c456df
Alena Torop <tor92@tut.by>
2022-02-12 22:41:10 +0300

    reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let newN = Math.abs(n);
+  let newArr = Array.from(newN.toString().split(''));
+	return Number(newArr.reverse().join(''));
 }
